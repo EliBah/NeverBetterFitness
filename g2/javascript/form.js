99e3208451a162fa9fa82884fdc6b76df6154109
Spencer <sjjoel25@gmail.com>
2015-02-28 12:33:55 -0800

    JavaScript setup for login/setup on main page

--- g2/javascript/form.js
+++ g2/javascript/form.js
@@ -1,8 +1,37 @@
-function user_id() {
-	var user = document.getElementById("user_id");
-	prompt("test");
-	if (user.value.length < 6) {
-		alert("Password must be longer than 6 characters");
-	}
-			
+function signUp() {	
+	//Sets up and creates the email field
+	var divholder = document.getElementById("email_holder");
+	var createform = document.createElement('input');	
+	createform.setAttribute("type", "email");
+	createform.setAttribute("name", "email");
+	createform.setAttribute("id", "email");
+	createform.setAttribute("required", "required");
+	createform.setAttribute("class", "formList");
+	
+	//Creates the email label
+	var createlabel = document.createElement('label');
+	createlabel.innerHTML = "E-Mail: ";
+	createlabel.setAttribute("for", "email");
+	createlabel.setAttribute("class", "formList");
+	createlabel.setAttribute("id", "email_label");
+
+	//Creates the line break
+	var linebreak = document.createElement('br');
+	linebreak.setAttribute("id", "email_br");
+	
+	//Adds all the objects to the page
+	divholder.appendChild(createlabel);
+	divholder.appendChild(linebreak);
+	divholder.appendChild(createform);		
+}
+
+function logIn() {
+	var parent = document.getElementById("email_holder");
+	var field = document.getElementById("email");
+	var label = document.getElementById("email_label");
+	var br = document.getElementById("email_br");
+	parent.removeChild(field);
+	parent.removeChild(label);
+	parent.removeChild(br);
+	
 }
