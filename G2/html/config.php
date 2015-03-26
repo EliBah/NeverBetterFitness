aa62ccfce7520e3430def4c1c4fa39dd6f66131c
Eli Bah <elimanbah@gmail.com>
2015-03-25 20:59:04 -0700

    prototype stuff

---
+++ G2/html/config.php
@@ -0,0 +1,8 @@
+<?php
+    define('DB_HOST', 'mysql14.000webhost.com');
+    define('DB_USER', 'a7391749_sa');
+    define('DB_PASSWORD', 'eliman320');
+    define('DB_DATABASE', 'a7391749_forum');
+    define('HOMEURL', 'index.php');
+?>
+
