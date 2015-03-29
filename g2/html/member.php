a4b1ec4b57c9ea8d93e546ddb1689f88ab5dd195
Spencer <sjjoel25@gmail.com>
2015-03-29 15:24:47 -0700

    Trying to implement php

--- g2/html/member.php
+++ g2/html/member.php
@@ -26,27 +26,66 @@
 
         
         <div id="content">
-            
+            <?php
+			//$name =  get name some how...
+				$userfile = file('users.txt');
+				include 'functions.php';
+				require_once('config.php');
+				session_start();
+
+				// Connect to server and select database.
+				mysql_connect(DB_HOST, DB_USER, DB_PASSWORD)or die("cannot connect");
+				mysql_select_db(DB_DATABASE)or die("cannot select DB");
+				$tbl_name="topic"; // Table name
+
+				// get value of id that sent from address bar
+				$id=$_GET['id'];
+
+				//Question 4d: Access info from the members table
+				$sql="SELECT * FROM $tbl_name, members WHERE id='$id'";
+				$result=mysql_query($sql);
+
+				$rows=mysql_fetch_array($result);
+				
+				//Question 4b: do not allow users to view topic is user is not logged in.
+				/*if (!isLoggedIn()){
+					echo "Please log in to view topic!";
+					exit(); */
+				
+				
+				
+				foreach($userfile as $line) 
+				{ 
+					if(strpos($line,$rows['login']) !== false) { //need to grab session name
+						// break apart the line by delimiters to get date/name/percentage
+							$str = $line;//
+							$info = explode("||", $str);// $info is an array
+						break;
+					} else {
+						echo "Error loading information.";
+					}
+				}
+			?>
 			<div id="column1">
 				<div id="userimage">
 				</div>
 
             <ul id="userstats">
-					<li class="list3">User Name: <div id ="name_holder"></div></li>
-					<li class="list3">Date Joined: <div id ="date_joined_holder"></div></li>
-					<li class="list3">Last Accessed: <div id ="date_accessed_holder"></div></li>
+					<li class="list3">User Name: <?php echo $info[0] ?></li>
+					<li class="list3">Date Joined: <?php echo $info[3] ?></li>
+					<li class="list3">Last Accessed: <?php echo $info[4] ?></li>
 				 </ul>
 
 			</div>
 			
 			<div id = "column2">
 				<div id="progress">
-					<progress value="22" max="100"></progress>
+					<progress value="<?php echo $info[8] ?>" max="100"></progress>
 				</div>
 
                 <div id="userprogress">
                     <p>
-                        Progress: 22% <div id="progress_holder"></div>
+                        Progress: <?php echo $info[8] ?>%</div>
                     </p>
                 </div>
 			</div>
