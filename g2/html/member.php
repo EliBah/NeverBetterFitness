58c00463a482ab3ec98ad879fc74714f4c84cb76
Spencer <sjjoel25@gmail.com>
2015-03-29 20:54:05 -0700

    php

--- g2/html/member.php
+++ g2/html/member.php
@@ -1,3 +1,42 @@
+<?php 
+	$userfile = file('users.txt');
+				include 'functions.php';
+				require_once('config.php');
+				session_start();
+
+				// Connect to server and select database.
+				mysql_connect(DB_HOST, DB_USER, DB_PASSWORD)or die("cannot connect");
+				mysql_select_db(DB_DATABASE)or die("cannot select DB");
+				$tbl_name="topic"; // Table name
+				
+				//Question 4b: do not allow users to view topic is user is not logged in.
+				if (!isLoggedIn()){
+					echo "Please log in to view topic!";
+					exit(); 
+				}
+				
+				// get value of id that sent from address bar
+				$id=$_GET['id'];
+
+				//Question 4d: Access info from the members table
+				$sql="SELECT * FROM $tbl_name, members WHERE id='$id'";
+				$result=mysql_query($sql);
+
+				$rows=mysql_fetch_array($result);
+				
+				foreach($userfile as $line) 
+				{ 
+					if(strpos($line,$rows['login']) !== false) { //need to grab session name
+						// break apart the line by delimiters to get date/name/percentage
+							$str = $line;//
+							$info = explode("||", $str);// $info is an array
+						break;
+					} else {
+						echo "Error loading information. ";
+					}
+				}
+				
+				?>
 <!DOCTYPE html>
 
 <html lang="en">
@@ -26,46 +65,6 @@
 
         
         <div id="content">
-            <?php
-			//$name =  get name some how...
-				$userfile = file('users.txt');
-				include 'functions.php';
-				require_once('config.php');
-				session_start();
-
-				// Connect to server and select database.
-				mysql_connect(DB_HOST, DB_USER, DB_PASSWORD)or die("cannot connect");
-				mysql_select_db(DB_DATABASE)or die("cannot select DB");
-				$tbl_name="topic"; // Table name
-
-				// get value of id that sent from address bar
-				$id=$_GET['id'];
-
-				//Question 4d: Access info from the members table
-				$sql="SELECT * FROM $tbl_name, members WHERE id='$id'";
-				$result=mysql_query($sql);
-
-				$rows=mysql_fetch_array($result);
-				
-				//Question 4b: do not allow users to view topic is user is not logged in.
-				if (!isLoggedIn()){
-					echo "Please log in to view topic!";
-					exit(); 
-				
-				
-				
-				foreach($userfile as $line) 
-				{ 
-					if(strpos($line,$rows['login']) !== false) { //need to grab session name
-						// break apart the line by delimiters to get date/name/percentage
-							$str = $line;//
-							$info = explode("||", $str);// $info is an array
-						break;
-					} else {
-						echo "Error loading information.";
-					}
-				}
-			?>
 			<div id="column1">
 				<div id="userimage">
 				</div>
